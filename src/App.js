dcc89f6b220549416d2f3f88859ff5255a93bcbe
icemaple77 <icemaple7@gmail.com>
2022-10-24 22:15:36 -0700

    123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UploadFiles from "./components/UploadFiles.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h4> 使用 React 搭建文件上传 Demo</h4>
+      <div className="content">
+        <UploadFiles />
+      </div>
     </div>
   );
 }
---
+++ src/http-common.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:1080",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
